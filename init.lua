68c9eb40483a367d79c4db4f304d7a0d649798de
Don <serfdon@gmail.com>
2025-04-06 20:47:46 -0700

    Added setting

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
+local mines = core.settings:get_bool("mymineshaft.use_mines", true)
+
 mymineshaft = {}
 dofile(minetest.get_modpath("mymineshaft").."/nodes.lua")
 dofile(minetest.get_modpath("mymineshaft").."/machine.lua")
 dofile(minetest.get_modpath("mymineshaft").."/register.lua")
---dofile(minetest.get_modpath("mymineshaft").."/builder.lua")
+if mines then
 dofile(minetest.get_modpath("mymineshaft").."/shafts.lua")
-
+end
 
 
 
